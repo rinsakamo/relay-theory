1fca5a8a55e2cedee22669ee7702bedd8143cfc0
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-22 09:23:57 +0900

    Import registry-mediated title countermodel

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -1,3 +1,4 @@
+import RelayTheory.OwnershipInstitutionalTitleCountermodel
 import RelayTheory.ProbeFutureClosure
 import RelayTheory.OperationalAgencyLocusCountermodel
 import RelayTheory.SkillCompetenceResourceOrthogonality
