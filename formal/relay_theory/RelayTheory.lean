54c83ecbdd13cf11c2bcf0635698d0bb77f00f3e
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-22 11:20:59 +0900

    Import Concept-like equivalence countermodel

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -1,3 +1,4 @@
+import RelayTheory.ConceptProbeEquivalenceCountermodel
 import RelayTheory.HabitSelectionTendencyCountermodel
 import RelayTheory.SkillCrossWorldTransferCountermodel
 import RelayTheory.RetentionStabilitySeparation
