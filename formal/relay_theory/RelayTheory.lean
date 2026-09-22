865a2778c71d40d64b2709d0361811fbb6693f4d
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-22 09:10:28 +0900

    Import Skill competence-resource discriminator

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -1,3 +1,4 @@
+import RelayTheory.SkillCompetenceResourceOrthogonality
 import RelayTheory.HierarchicalSkillInterventionCountermodel
 import RelayTheory.OwnershipAuthorshipControlCountermodel
 import RelayTheory.OperationalSkillCountermodel
