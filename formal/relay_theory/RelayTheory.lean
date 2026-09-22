35fdcfec6925dfaba89391de20df0c558d278312
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-22 09:46:34 +0900

    formal: import temporal order metric-independence witness

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -21,6 +21,7 @@ import RelayTheory.LocalPresentReindexing
 import RelayTheory.TemporalOrientationAsymmetry
 import RelayTheory.LocalLineageGlobalOrderCountermodel
 import RelayTheory.GlobalOrderSymmetryObstruction
+import RelayTheory.TemporalOrderMetricIndependence
 
 /-!
 # RelayTheory formal scaffold
