9376e199a7eeb17959306cc783e6b475646d9ca4
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-22 09:05:05 +0900

    formal: import rebased local-lineage countermodel

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -10,6 +10,7 @@ import RelayTheory.LossyWorkload
 import RelayTheory.PresentSliceCountermodel
 import RelayTheory.LocalPresentReindexing
 import RelayTheory.TemporalOrientationAsymmetry
+import RelayTheory.LocalLineageGlobalOrderCountermodel
 
 /-!
 # RelayTheory formal scaffold
